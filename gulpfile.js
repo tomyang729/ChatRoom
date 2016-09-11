048bb038871c4831e5cb22ae23ad7a6ca972c2bb
tomyang729 <tomatoyang729@gmail.com>
2016-09-11 13:41:55 -0700

    added gulp-browserSync

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,24 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 gulp.task('sass', function () {
     return gulp.src('./public/sass/main.scss')
         .pipe(sass())  //using gulp-sass
         .pipe(gulp.dest('./public/stylesheets/'))
+        .pipe(browserSync.reload({
+            stream: true
+        }))
 });
 
-gulp.task('watch', function () {
-    gulp.watch('./public/sass/*.scss', ['sass']);
+gulp.task('browserSync', function () {
+    browserSync.init({
+        proxy: "localhost:3000"
+    })
 });
+
+gulp.task('watch', ['browserSync', 'sass'], function () {
+    gulp.watch('./public/sass/*.scss', ['sass']);
+    // other watches
+});
+
